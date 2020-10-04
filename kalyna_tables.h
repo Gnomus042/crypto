e224f9f40f6e70f27030f8c2e92104030595761f
Anastasiia Byvsheva <gnomus042@gmail.com>
2020-10-05 00:43:48 +0300

    Some refactoring and primary AES tests

---
+++ kalyna_tables.h
@@ -0,0 +1,20 @@
+//
+// Created by anast on 10/5/2020.
+//
+
+#ifndef CRYPTO_KALYNA_TABLES_H
+#define CRYPTO_KALYNA_TABLES_H
+
+#endif //CRYPTO_KALYNA_TABLES_H
+
+#include "helper.h"
+
+vector<Byte> sbox0;
+vector<Byte> sbox1;
+vector<Byte> sbox2;
+vector<Byte> sbox3;
+
+vector<Byte> isbox0;
+vector<Byte> isbox1;
+vector<Byte> isbox2;
+vector<Byte> isbox3;
--- test/test.cpp
+++ test/test.cpp
@@ -3,8 +3,60 @@
 //
 
 #include <gtest/gtest.h>
+#include "../AES.h"
 
 int main(int argc, char ** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
+}
+
+TEST (AES, 128_key)
+{
+    string key_str = "1234567890123456";
+    string data_str = "Absolutely random sentence";
+
+    vector<uint8_t> key(begin(key_str), end(key_str));
+    vector<uint8_t> data(begin(data_str), end(data_str));
+
+    AES aes(key);
+    string res = "";
+    for (uint8_t q: aes.decrypt(aes.encrypt(data))) {
+           res += (char)q;
+    }
+    res = res.substr(0, data.size());
+    EXPECT_EQ(res, data_str);
+}
+
+TEST (AES, 256_key)
+{
+    string key_str = "12345678901234561234567890123456";
+    string data_str = "Absolutely random sentence";
+
+    vector<uint8_t> key(begin(key_str), end(key_str));
+    vector<uint8_t> data(begin(data_str), end(data_str));
+
+    AES aes(key);
+    string res = "";
+    for (uint8_t q: aes.decrypt(aes.encrypt(data))) {
+        res += (char)q;
+    }
+    res = res.substr(0, data.size());
+    EXPECT_EQ(res, data_str);
+}
+
+TEST (AES, 192_key)
+{
+    string key_str = "123456789012345612345678";
+    string data_str = "Absolutely random sentence";
+
+    vector<uint8_t> key(begin(key_str), end(key_str));
+    vector<uint8_t> data(begin(data_str), end(data_str));
+
+    AES aes(key);
+    string res = "";
+    for (uint8_t q: aes.decrypt(aes.encrypt(data))) {
+        res += (char)q;
+    }
+    res = res.substr(0, data.size());
+    EXPECT_EQ(res, data_str);
 }
